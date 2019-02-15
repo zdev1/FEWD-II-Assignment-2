d89b38801bd40261afa2799f8196af184c00da7e
zdev1 <46204727+zdev1@users.noreply.github.com>
2019-02-14 21:33:31 -0800

    started assignment
    started assignment

---
+++ Zimmerman_assignment2/js/02-coinflip.js
@@ -0,0 +1,23 @@
+/*eslint-env browser*/
+
+
+var coinFlip = Math.round(Math.random()) + 1;
+var choice = prompt("Heads or Tails");
+if (coinFlip == 1) {
+    var flipResult = "heads";
+} else {
+    var flipResult = "tails";
+}
+if (flipResult == choice) {
+    if (flipResult == "heads") {
+        alert("The flip was heads and you chose heads...you win!");
+    } else {
+        alert("The flip was tails and you chose tails...you win!");
+    }
+} else {
+    if (flipResult == "heads") {
+        alert("The flip was heads and you chose tails...you lose!");
+    } else {
+        alert("The flip was tails and you chose heads...you lose!");
+    }
+}
---
+++ Zimmerman_assignment2/js/03-coinflip-redux.js
@@ -0,0 +1,9 @@
+/*eslint-env browser*/
+
+var coinFlip;
+
+        for  (coinFlip =Math.random(; coinFlip <= 10; coinFlip+= Math.round) {
+            
+      
+        }
+        var @customer_id = 13;
